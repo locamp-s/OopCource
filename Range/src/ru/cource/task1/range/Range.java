89a45c700cf2d5b8da2506ee26e16fc9e4c8b4ae
Sofya <olympics2012love@gmail.com>
2022-01-17 20:13:08 +0700

    Добавление методов для нахождения длины диапазона

--- Range/src/ru/cource/task1/range/Range.java
+++ Range/src/ru/cource/task1/range/Range.java
@@ -1,4 +1,36 @@
 package ru.cource.task1.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public double GetLength(double from, double to) {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        return number >= this.from && number <= this.to;
+    }
 }
--- Range/src/ru/cource/task1/range_main/Main.java
+++ Range/src/ru/cource/task1/range_main/Main.java
@@ -1,7 +1,26 @@
 package ru.cource.task1.range_main;
 
+import ru.cource.task1.range.Range;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите начало диапазона: ");
+        double from = scanner.nextDouble();
+
+        System.out.println("Введите конец диапазона: ");
+        double to = scanner.nextDouble();
+
+        System.out.println("Введите число для проверки: ");
+        double number = scanner.nextDouble();
+
+        Range range = new Range(from, to);
+
+        System.out.println("Длина диапазона: " + range.GetLength(from, to));
+        System.out.println("Принадлежит ли число диапазону: " + range.isInside(number));
+
     }
 }
